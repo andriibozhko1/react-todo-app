ffd65a2a5e4f7950e4776cd7103848fb8ef09013
Andrii <andriibozhko1@gmail.com>
2019-02-26 19:30:26 +0200

    to gh-pages

---
+++ precache-manifest.9f4fcb22ae099e674d2affe00a3dda24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80c5e87b521abe4f91be",
+    "url": "/react-todo-app/static/css/main.77aa631f.chunk.css"
+  },
+  {
+    "revision": "80c5e87b521abe4f91be",
+    "url": "/react-todo-app/static/js/main.80c5e87b.chunk.js"
+  },
+  {
+    "revision": "0945be15e27dd30a16be",
+    "url": "/react-todo-app/static/js/runtime~main.0945be15.js"
+  },
+  {
+    "revision": "a080abdb6f85b3109260",
+    "url": "/react-todo-app/static/js/2.a080abdb.chunk.js"
+  },
+  {
+    "revision": "d8463adc991eca6e5d62dab6164ab12b",
+    "url": "/react-todo-app/index.html"
+  }
+];
